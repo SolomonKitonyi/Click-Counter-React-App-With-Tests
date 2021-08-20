fba0e8316b40a37f326ae3a376dd2d49ab67c61c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-08-20 09:08:28 +0300

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("Renders without error", () => {});
+
+test("Renders increment button", () => {});
+
+test("Renders counter display", () => {});
+
+test("Counter starts at 0", () => {});
+
+test("Clicking button increments display", () => {});
