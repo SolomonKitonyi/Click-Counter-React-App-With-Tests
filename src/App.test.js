6afb050bd70ccc9d75e6708a6055ac54b1f60e28
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-08-23 09:06:58 +0300

    add initail counter test

--- src/App.test.js
+++ src/App.test.js
@@ -39,6 +39,10 @@ test("Renders counter display", () => {
   expect(counterDisplay.length).toBe(1);
 });
 
-test("Counter starts at 0", () => {});
+test("Counter starts at 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
 
 test("Clicking button increments display", () => {});
