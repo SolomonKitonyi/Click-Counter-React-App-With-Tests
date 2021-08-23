7ab1ddfbda6f7db9425f96e72e149e1c4ae091ec
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-08-23 09:06:04 +0300

    refractor to class based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently</h1>
-      <button data-test="increment-button">Increment counter</button>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment counter</button>
+      </div>
+    );
+  }
+}
 
 export default App;
